22972a1c4b71a63b92ff6e09aa46d43852683049
Fran <francocaminosmail@gmail.com>
2023-03-03 09:12:44 -0300

    products service fixed

--- src/services/products.js
+++ src/services/products.js
@@ -1,21 +1,28 @@
 import axios from "axios";
 import products from "@/models/products";
 
-class service {
-  getAllProducts() {
-    const url = "https://fakestoreapi.com/products";
-
-    return axios.get(`${url}`).then(({ data }) => {
-      return data.map((item) => {});
-      return products.fromJson(item);
-    });
-  }
-  getAllCategories() {
-    const url = "https://fakestoreapi.com/products/categories";
-    return axios.get(`${url}`).then(({ data }) => {
-      return data;
-    });
-  }
+class service{
+  //traigo los productos de la API
+    getAllProducts() {
+        const url = 'https://fakestoreapi.com/products'
+        
+           return axios
+             .get(`${url}`)
+             .then(({ data }) => {
+               return data.map((item) => {
+                 return products.fromJson(item);
+               });
+             });
+    }
+    //traigo las categorias de la api
+    getAllCategories(){
+      const url = 'https://fakestoreapi.com/products/categories'
+      return axios
+      .get(`${url}`)
+      .then(({ data }) => {
+        return data
+      });
+    }
 }
 
 export default service;
